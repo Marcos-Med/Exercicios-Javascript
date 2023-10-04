47fdcb90c39dff231e48a43aa3fc55dcefbea773
Marcos <marcos.mb.mb@outlook.com>
2023-10-04 20:29:16 -0300

    Exercicio One

---
+++ exec_1.js
@@ -0,0 +1,10 @@
+const operacoes = (number_1 = 0, number_2 = 1) => {
+    console.log(`${number_1} + ${number_2} = ${number_1 + number_2}`);
+    console.log(`${number_1} - ${number_2} = ${number_1 - number_2}`);
+    console.log(`${number_1} * ${number_2} = ${number_1 * number_2}`);
+    console.log(`${number_1} / ${number_2} = ${number_1 / number_2}`);
+}
+
+operacoes(2,3);
+operacoes(3,3);
+operacoes(3,4);
